c05805f30b267a891b36a23ee073682a6654e068
tomwang <townwang@retronix.com.tw>
2024-09-02 10:35:47 +0800

    ARM: dts: configs: Add Raptor PCBA.
    Signed-off-by: Tom Wang <townwang@retronix.com.tw>
    Signed-off-by: Felix Hsu <felixhsu@retronix.com.tw>

---
+++ include/configs/raptor.h
@@ -0,0 +1,24 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * include/configs/raptor.h
+ *     This file is Raptor board configuration.
+ *
+ * Copyright (C) 2021 Renesas Electronics Corp.
+ */
+
+#ifndef __RAPTOR_H
+#define __RAPTOR_H
+
+#include "rcar-gen4-common.h"
+
+/* Ethernet RAVB */
+#define CONFIG_BITBANGMII_MULTI
+
+/* Board Clock */
+/* XTAL_CLK : 16.66MHz */
+#define CONFIG_SYS_CLK_FREQ	16666666u
+
+/* Generic Timer Definitions (use in assembler source) */
+#define COUNTER_FREQUENCY	0xFE502A	/* 16.66MHz from CPclk */
+
+#endif /* __RAPTOR_H */
